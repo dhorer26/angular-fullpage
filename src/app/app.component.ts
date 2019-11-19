0cce32fa066dbed58f863c13708a33744d2a3aff
Ravindra Dhore <ravindrad@ansh.com>
2019-11-19 14:12:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'firebase-auth-angular-app';
+  // tslint:disable-next-line: variable-name
+  fullpage_api: any;
+  config: any;
+  constructor() {
+    // for more details on config options please visit fullPage.js docs
+    this.config = {
+
+      // fullpage options
+      licenseKey: 'YOUR LICENSE KEY HERE',
+      anchors: ['firstPage', 'secondPage', 'thirdPage'],
+      menu: '#menu',
+
+      // fullpage callbacks
+      afterResize: () => {
+        console.log('After resize');
+      },
+      afterLoad: (origin: any, destination: any, direction: any) => {
+        console.log(origin.index);
+      }
+    };
+  }
+  getRef(fullPageRef: any) {
+    this.fullpage_api = fullPageRef;
+  }
 }
